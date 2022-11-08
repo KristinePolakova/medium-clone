1a585fec8bd6bfebbca01024f2887268bd69fa37
Polakova <kristine.polakova@accenture.com>
2022-11-08 14:18:22 +0200

    Added shared components - banner,errorMessage and loading

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -7,6 +7,8 @@ import { GetFeedEffect } from './store/effects/getFeed.effect';
 import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/reducers';
 import { RouterModule } from '@angular/router';
+import { ErrorMessageModule } from '../errorMessage/error-message.module';
+import { LoadingModule } from '../loading/loading.module';
 
 @NgModule({
   declarations: [FeedComponent],
@@ -15,6 +17,8 @@ import { RouterModule } from '@angular/router';
     EffectsModule.forFeature([GetFeedEffect]),
     StoreModule.forFeature('feed', reducers),
     RouterModule,
+    ErrorMessageModule,
+    LoadingModule,
   ],
   exports: [FeedComponent],
   providers: [FeedService],
